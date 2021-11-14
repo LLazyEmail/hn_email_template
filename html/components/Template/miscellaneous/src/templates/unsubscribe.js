1d19246f49880d0cc07a9c9804731ec223c36090
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-11-14 13:37:34 +0000

    updates

--- html/components/Template/miscellaneous/src/templates/unsubscribe.js
+++ html/components/Template/miscellaneous/src/templates/unsubscribe.js
@@ -6,3 +6,14 @@ unsubscribe
 </a>`;
 
 
+const unsubscribeComponent = (unsubscribe) => {
+    return `<a href="${unsubscribe}"
+    style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #656565;font-weight: normal;text-decoration: underline;">
+    unsubscribe
+    </a>`;
+}
+
+
+
+
+export { unsubscribe, unsubscribeComponent };
