d09f7b619dc3192460e7280bcbd1b56f816a80d4
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-11-20 14:53:57 +0000

    debugging a thing - soomething didnt work well

--- html/components/Template/miscellaneous/src/index.js
+++ html/components/Template/miscellaneous/src/index.js
@@ -1,7 +1,7 @@
 import { address, addressComponent } from './templates/address';
 import { copyrights, copyrightsComponent } from './templates/copyrights';
 import { fonts, fontsComponent } from './templates/fonts';
-import { headStyles, headStylesComponent } from './templates/head-styles';
+// import { headStyles, headStylesComponent } from './templates/head-styles';
 import { newsletterSponsorshipLink, newsletterSponsorshipLinkComponent } from './templates/newsletter-sponsorship-link';
 import { unsubscribe, unsubscribeComponent } from './templates/unsubscribe';
 
@@ -9,7 +9,7 @@ const misc = {
   address,
   copyrights,
   fonts,
-  headStyles,
+  // headStyles,
   newsletterSponsorshipLink,
   unsubscribe,
 
@@ -17,7 +17,7 @@ const misc = {
   addressComponent,
   copyrightsComponent,
   fontsComponent,
-  headStylesComponent,
+  // headStylesComponent,
   newsletterSponsorshipLinkComponent,
   unsubscribeComponent,
 
