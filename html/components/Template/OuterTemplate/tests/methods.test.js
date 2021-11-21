49902742674556ff090212b5f97586f6d936b8cf
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-11-21 13:58:20 +0000

    updates

--- html/components/Template/OuterTemplate/tests/methods.test.js
+++ html/components/Template/OuterTemplate/tests/methods.test.js
@@ -2,49 +2,62 @@
 
 const { printMain, printFooter, printBody } = require('../src/methods');
 
+// var generateEmptyTemplateComponent = require('../src/templates/emptyTemplate');
+
+// var generateTemplateComponent = require('../src/templates/generateTemplate');
+
+
 describe('test helpers', () => {
-  test('rendering Main Component', () => {
+
+  // test('rendering Empty Template', () => {
+
+  //   const string = generateEmptyTemplateComponent();
+  //   console.log(string);
+  // });
 
 
-      const string = printMain();
-
-      console.log(string);
-
-      expect(printMain()).toBeDefined();
-
-      // expect(typeof string).toBe('string');
-
-  });
+  // test('rendering Main Component', () => {
 
 
-  test('rendering Body Component', () => {
+  //     const string = printMain();
+
+  //     console.log(string);
+
+  //     expect(printMain()).toBeDefined();
+
+  //     // expect(typeof string).toBe('string');
+
+  // });
 
 
-      const string = printBody();
-
-      console.log(string);
-
-      expect(printMain()).toBeDefined();
-
-      // expect(typeof string).toBe('string');
+  // test('rendering Body Component', () => {
 
 
-  });
+  //     const string = printBody();
+
+  //     console.log(string);
+
+  //     expect(printMain()).toBeDefined();
+
+  //     // expect(typeof string).toBe('string');
 
 
-  test('rendering Footer Component', () => {
+  // });
 
 
-    const string = printFooter();
-
-    console.log(string);
-
-    expect(printMain()).toBeDefined();
-
-    // expect(typeof string).toBe('string');
+  // test('rendering Footer Component', () => {
 
 
-  });
+  //   const string = printFooter();
+
+  //   console.log(string);
+
+  //   expect(printMain()).toBeDefined();
+
+  //   // expect(typeof string).toBe('string');
+
+
+  // });
 
 });
 
