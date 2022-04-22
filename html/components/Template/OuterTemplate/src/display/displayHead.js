440c03d13183123ab715471be14f82a5a80307c7
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-22 20:58:32 +0300

    Update displayHead.js

--- html/components/Template/OuterTemplate/src/display/displayHead.js
+++ html/components/Template/OuterTemplate/src/display/displayHead.js
@@ -1,13 +1,26 @@
 // partials
-import { Head } from '';
+import misc from 'atherdon-newsletter-js-layouts-misc';
+import { headStyles } from '.../templates/head-styles';
+import { headComponent } from '../templates/head';
+
+const { fonts } = misc;
+const title = `The Secrets of High-Performing DevOps teams`;
+
+
 
 const DDDisplayHead = {
-  component: Head,
+  component: headComponent,
   params: { title, headStyles, fonts },
   display: () => {
+    return this.component(....)
   },
   checks: () => []
   
 }
 
 export default DDDisplayHead;
+
+
+//const displayHead = (title) => {
+//  return headComponent(title, headStyles, fonts);
+//}
