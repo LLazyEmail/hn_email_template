54619e5a11292ed4c86ae60ed28c2e72a054dfc8
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-22 21:01:34 +0300

    Update displayMain.js

--- html/components/Template/OuterTemplate/src/display/displayMain.js
+++ html/components/Template/OuterTemplate/src/display/displayMain.js
@@ -1,6 +1,9 @@
 // partials
 import { MainComponent } from '';
 
+// note that head and body params are actually `displayHead` & `displayBody` 
+// methods for sub-components
+
 const DDDisplayMain = {
   component: MainComponent,
   params: { head, body },
@@ -12,3 +15,26 @@ const DDDisplayMain = {
 }
 
 export default DDDisplayMain;
+
+
+
+const {
+    address,
+    copyrights,
+    
+    newsletterSponsorshipLink,
+    unsubscribe,
+
+    fonts
+} = misc;
+// import { headStyles } from '../templates/head-styles';
+const title = `The Secrets of High-Performing DevOps teams`;
+
+const displayMain = () => {
+  
+    const head = displayHead(title);
+    const body = displayBody();
+    
+    return mainComponent(head, body);
+}
+
