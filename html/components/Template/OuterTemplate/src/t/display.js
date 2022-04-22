c14ccd9fd7abcd068a89c988604dd00840a02e98
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-22 21:04:15 +0300

    Update display.js

--- html/components/Template/OuterTemplate/src/t/display.js
+++ html/components/Template/OuterTemplate/src/t/display.js
@@ -61,12 +61,6 @@ const displayBody = () => {
 }
 
 
-// {
-//     title,
-//     bodyContent,
-//     previewText,
-//     headCSS = '',
-// }
 const renderTemplate = (options) => {
 
     //create options
@@ -74,10 +68,7 @@ const renderTemplate = (options) => {
 
 
 
-
     // return 
-
-
 
 }
 
@@ -112,30 +103,3 @@ const configValidation = (options) => {
 // }) => {
 
 
-
-// const ERROR_TITLE = '`title` is a required option for `renderTemplate`'
-const checkingTitle = (title) => {
-    if (!title) {
-        throw new Error('`title` is a required option for `renderTemplate`');
-    }
-}
-
-// const ERROR_BODY = '`bodyContent` is a required option for `renderTemplate`';
-const checkingBodyContent = (bodyContent) => {
-    if (!bodyContent) {
-        throw new Error('`bodyContent` is a required option for `renderTemplate`');
-    }
-}
-
-//const ERROR_PREVIEW = '`previewText` is a required option for `renderTemplate`';
-const checkingPreviewText = (previewText) => {
-    if (!previewText) {
-        throw new Error('`previewText` is a required option for `renderTemplate`');
-    }
-}
-  
-export {
-    checkingTitle,
-    checkingBodyContent,
-    checkingPreviewText
-}
