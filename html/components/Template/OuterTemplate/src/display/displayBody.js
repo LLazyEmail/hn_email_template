69b8995e3256387f685091b585a58c7860e68169
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-22 21:02:56 +0300

    Update displayBody.js

--- html/components/Template/OuterTemplate/src/display/displayBody.js
+++ html/components/Template/OuterTemplate/src/display/displayBody.js
@@ -1,6 +1,15 @@
 // partials
 import { Body } from '';
 
+// const ERROR_BODY = '`bodyContent` is a required option for `renderTemplate`';
+const checkingBodyContent = (bodyContent) => {
+    if (!bodyContent) {
+        throw new Error('`bodyContent` is a required option for `renderTemplate`');
+    }
+}
+
+// note that footer param here is a subcomponent, 
+// so we passing footerDisplay instead of just a component
 const DDDisplayBody = {
   component: Body,
   params: { footer, logoTop, logoBottom, content },
