b07c2357f67b45a93f8f392c1441980b936e887e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-22 17:14:36 +0300

    Create displayMain.js

---
+++ html/components/Template/OuterTemplate/src/display/displayMain.js
@@ -0,0 +1,15 @@
+// partials
+import { MainComponent } from '';
+
+const DDDisplayMain = {
+  component: MainComponent,
+  params: {
+  },
+  display: () => {
+  },
+  checks: () => {
+  }
+  
+}
+
+export default DDDisplayMain;
