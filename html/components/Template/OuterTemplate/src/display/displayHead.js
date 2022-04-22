d94d7af07a61190dcb95f8a1c3e1a45c30d5dfcd
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-22 17:15:54 +0300

    Create displayHead.js

---
+++ html/components/Template/OuterTemplate/src/display/displayHead.js
@@ -0,0 +1,15 @@
+// partials
+import { Head } from '';
+
+const DDDisplayHead = {
+  component: Head,
+  params: {
+  },
+  display: () => {
+  },
+  checks: () => {
+  }
+  
+}
+
+export default DDDisplayHead;
