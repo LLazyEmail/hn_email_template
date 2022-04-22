e6a80975a776b84c52e07ee122e4c7e6ea32d7a5
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-22 21:01:55 +0300

    Update displayHead.js

--- html/components/Template/OuterTemplate/src/display/displayHead.js
+++ html/components/Template/OuterTemplate/src/display/displayHead.js
@@ -6,7 +6,12 @@ import { headComponent } from '../templates/head';
 const { fonts } = misc;
 const title = `The Secrets of High-Performing DevOps teams`;
 
-
+// const ERROR_TITLE = '`title` is a required option for `renderTemplate`'
+const checkingTitle = (title) => {
+    if (!title) {
+        throw new Error('`title` is a required option for `renderTemplate`');
+    }
+}
 
 const DDDisplayHead = {
   component: headComponent,
