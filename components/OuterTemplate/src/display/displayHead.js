40b271667a8eeae07714370bdd0d576f3b4fd501
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-05-03 15:40:51 +0300

    Update displayHead.js

--- components/OuterTemplate/src/display/displayHead.js
+++ components/OuterTemplate/src/display/displayHead.js
@@ -1,9 +1,13 @@
+// @TODO we need to finalize this file, so we can accept real data, not just static title...
+
 // partials
 import misc from 'atherdon-newsletter-js-layouts-misc';
-import { headStyles } from '.../components/head-styles';
+import { headStyles } from '../components/head-styles';
+
 import headComponent from '../components/head';
 
 const { fonts } = misc;
+
 const title = `The Secrets of High-Performing DevOps teams`;
 
 // const ERROR_TITLE = '`title` is a required option for `renderTemplate`'
@@ -17,15 +21,17 @@ const DisplayHead = {
   component: headComponent,
   params: { title, headStyles, fonts },
   display: () => {
-    return this.component(....)
+      
+//         return headComponent(title, headStyles, fonts);
+      
+      console.log( this.component(....) );
+//     return this.component(....)
+      
   },
-  checks: () => []
+  checks: () => [
+      checkingTitle(this.params.title)
+  ]
   
 }
 
 export default DisplayHead;
-
-
-//const displayHead = (title) => {
-//  return headComponent(title, headStyles, fonts);
-//}
