b96ae766b186f5e2796240423b3aada31f50dd00
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-20 02:29:32 +0300

    Create innerContentComponent.js

---
+++ src/components/innerContentComponent.js
@@ -0,0 +1,10 @@
+// Hey, this component was created only for testing purposes. it wouldnt be returned or used at all.
+
+function innerContentComponent = () => {
+  
+   return ``; 
+  
+}
+
+
+export default innerContentComponent;
