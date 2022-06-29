220d8931e239f6015c22283b9492d6953fff101b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-29 20:08:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-// import innerComponents from 'atherdon-newsletter-js-layouts-body';
 
 import { printMain, printFooter, printBody } from './methods';
-
-// import fullTemplateOLD from './depricated/full-template';
-// import fullTemplate from './depricated/tempFT';
-
-// console.log('123');
 
 // import generateTemplateComponent from './t/generateTemplate'
 // import generateEmptyTemplateComponent from './t/emptyTemplate'
@@ -13,25 +7,8 @@ import { printMain, printFooter, printBody } from './methods';
 export default {
   //   body,
 
-  //   //spread misc
-  //   address,
-  //   copyrights,
-  //   fonts,
-  //   headStyles,
-  //   newsletterSponsorshipLink,
-  //   unsubscribe,
-  //   head,
-
   // fullTemplateOLD,
   // fullTemplate,
-
-  // headline,
-  // logoBottom,
-  // logoTop,
-  // previewText,
-  // section,
-  // sponsor,
-  // ctaList,
 
   // generateTemplateComponent,
   // generateEmptyTemplateComponent,
