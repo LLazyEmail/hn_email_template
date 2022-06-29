592abd3c6cd7c192bab725403ca3c02f2942fd9a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-29 06:15:39 +0300

    Update XdisplayContent.js

--- src/display/XdisplayContent.js
+++ src/display/XdisplayContent.js
@@ -1,1 +1,14 @@
 //---------------
+// ---------------
+import displayFactoryTwo from './factoryTwo';
+
+// const settings = {
+//     component: '',
+
+//     params: {},
+
+// }
+
+// const TemplateFactory = new displayFactoryTwo();
+
+// export default TemplateFactory.create(settings);
