9c485b96507f27b0ea85ce6423ae4c308ca31353
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-18 13:56:19 +0300

    Update unsubscribe.js

--- components/Miscellaneous/src/components/unsubscribe.js
+++ components/Miscellaneous/src/components/unsubscribe.js
@@ -1,11 +1,4 @@
-// import { config } from '../config';
-
-// const unsubscribe = `<a href="${config.unsubscribe}"
-// style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #656565;font-weight: normal;text-decoration: underline;">
-// unsubscribe
-// </a>`;
-
-const unsubscribeComponent = (unsubscribe) => {
+const unsubscribeComponent = ({unsubscribe}) => {
   if (!unsubscribe) new Error('invalid unsubscribe');
 
   return `<a href="${unsubscribe}"
