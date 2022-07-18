5204ef5b1616f1bd5bcb7bc57a511962c4557fdd
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-18 13:55:48 +0300

    Update newsletter-sponsorship-link.js

--- components/Miscellaneous/src/components/newsletter-sponsorship-link.js
+++ components/Miscellaneous/src/components/newsletter-sponsorship-link.js
@@ -1,11 +1,4 @@
-// import { config } from '../config';
-
-// const newsletterSponsorshipLink =
-//
-// <a href="${config.contact}" target="_blank" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #656565;font-weight: normal;text-decoration: underline;">
-//   Click Here To`;
-
-const newsletterSponsorshipLinkComponent = (contact) => {
+const newsletterSponsorshipLinkComponent = ({contact}) => {
   if (!contact) {
     throw new Error('no contact was passed');
   }
