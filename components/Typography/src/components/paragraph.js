ac37e0cede4be7b2d1ff6848d6b30cc2d51b8966
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-18 14:00:19 +0300

    Update paragraph.js

--- components/Typography/src/components/paragraph.js
+++ components/Typography/src/components/paragraph.js
@@ -1,5 +1,4 @@
-
-const paragraphComponent = (content) => {
+const paragraphComponent = ({content}) => {
     return `<div dir="ltr" style="text-align: justify;"><span style="font-size:16px"><span style="font-family:trebuchet ms,lucida grande,lucida sans unicode,lucida sans,tahoma,sans-serif">${content}</span></span></div>`;
 }
 
