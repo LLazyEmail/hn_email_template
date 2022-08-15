04bbc5e8487e02f27d51c9f8d0dff77da6160163
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-08-15 10:36:01 +0000

    upd

--- src/display/displayContent.js
+++ src/display/displayContent.js
@@ -2,6 +2,11 @@ import { displayFactoryTwo } from 'email-template-object';
 
 import innerContentComponent from '../components/';
 
+// import { buttonComponent } from 'atherdon-newsletter-js-layouts-typography';
+
+// console.log(buttonComponent({href:'https://google.com', content: 'this is a link'}));
+
+
 //variant one
 const settings = {
   component: innerContentComponent,
