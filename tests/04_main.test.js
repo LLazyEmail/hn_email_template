cd0d373c11539fdcb632afcb5e7669055a7d7400
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-27 02:22:17 +0200

    Update and rename 05_main.test.js to 04_main.test.js

--- tests/04_main.test.js
+++ tests/04_main.test.js
@@ -1,8 +1,6 @@
 import { writingFile } from 'markup-generator';
 
-// import { MainHTMLString } from '../src/display/displayMain';
-
-import MainHTMLString from '../src/display/displayMain';
+import { MainHTMLString } from '../src/display/displayMain';
 
 
 describe('main display method must be initialized', () => {
