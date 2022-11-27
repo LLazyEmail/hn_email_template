97a5704f2790098c8a4b9f4e70daa56fee6a658f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-27 02:03:07 +0200

    Update 00_head.test.js

--- tests/00_head.test.js
+++ tests/00_head.test.js
@@ -1,1 +1,28 @@
-xxx
+// import { writingFile } from 'markup-generator';
+
+// import HeadHTMLString from '../src/display/displayHead';
+
+
+// describe('head display method must be initialized', () => {
+  
+//      test('display head', () => {
+//       expect(typeof HeadHTMLString === 'string').toBe(true);
+//     });
+
+  
+// });
+
+
+// describe('writing components into files', () => {
+  
+  
+//   test('write head', () => {
+//     const string1 = HeadHTMLString;
+
+//     // console.log(string);
+//     expect(string1).toBeDefined();
+
+//     writingFile(string1, 'nmtg');
+//   });
+  
+// });
